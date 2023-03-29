794d8d083f9068a1fed0ccbd823d1011ce063fe0
izadoranetz <izadora.ns@gmail.com>
2023-03-29 08:46:32 -0300

    routes and nodemon

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+
+                        // req = request, res = response
+app.get("/", function(request, response){
+    response.send("Hello World!");
+})
+
+app.get("/contact", function(req, res){
+    res.send("Contact me at: myemaul@email");
+})
+
+app.get("/about", function(req, res){
+    res.send("Hi! I'm Iza, a designer and developer.");
+})
+
+app.get("/etc", function(req, res){
+    res.send("Bla bla bla.");
+})
+
+app.listen(3001, () => {
+    console.log('Server started on port 3001');
 });
